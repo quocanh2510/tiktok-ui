016f1f9f6307c54b7ef68fb2d38901e29add176a
quocanh2510 <voquocanh251098@gmail.com>
2022-08-29 17:18:38 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/routes/index.js
@@ -0,0 +1,38 @@
+//Layouts
+import HeaderOnly from '~/components/Layout/HeaderOnly';
+
+//Pages
+import HomePage from '~/pages/Home';
+import FollowingPage from '~/pages/Following';
+import ProfilePage from '~/pages/Profile';
+import UploadPage from '~/pages/Upload';
+import SearchPage from '~/pages/Search';
+
+const publicRoute = [
+    {
+        path: '/',
+        component: HomePage,
+    },
+    {
+        path: '/following',
+        component: FollowingPage,
+    },
+    {
+        path: '/profile',
+        component: ProfilePage,
+    },
+    {
+        path: '/upload',
+        component: UploadPage,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: SearchPage,
+        layout: null,
+    },
+];
+
+const privateRoute = [];
+
+export { publicRoute, privateRoute };
